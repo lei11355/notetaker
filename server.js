6ab4f80e3f06a89fd144af451fcbc33b2d5ce49d
lei11355 <lei11355@gmail.com>
2022-03-20 13:38:45 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+
+const PORT = process.env.PORT || 3004;
+const app = express();
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
+  
